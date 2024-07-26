eacb64add1edf9ff4962404f0f531391c2598faa
Arthur Azevedo Da Silva <107653383+Tutuacs@users.noreply.github.com>
2024-07-26 19:01:36 -0300

    discoment session omg

--- client/src/app/(user)/layout.tsx
+++ client/src/app/(user)/layout.tsx
@@ -9,11 +9,11 @@ interface PrivateLayoutProps {
 }
 
 export default async function UserLayout({ children }: PrivateLayoutProps) {
-  // const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-  // if (!session) {
-  //   redirect("/");
-  // }
+  if (!session) {
+    redirect("/");
+  }
 
   return (
     <>
