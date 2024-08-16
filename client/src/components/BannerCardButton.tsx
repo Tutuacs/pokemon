dc26536c466b1e436e117000b09e8b4fa8ce7abf
Arthur Azevedo Da Silva <107653383+Tutuacs@users.noreply.github.com>
2024-08-15 22:00:26 -0300

    Atualizar o BannerCardButton.tsx
    change from static var to variable

--- client/src/components/BannerCardButton.tsx
+++ client/src/components/BannerCardButton.tsx
@@ -6,13 +6,11 @@ import { useNavbarContext } from "./NavBarProviders";
 export default function BannerCardButton() {
   const { profile } = useNavbarContext();
 
-  const profileOK = profile ? (profile.normalRolls > 0 ? true : false) : false;
-
   return (
     <main>
       <div className="flex justify-end p-4">
         <Link
-          href={profileOK ? "/roll/stellar" : ""}
+          href={profile ? (profile.normalRolls > 0 ? "/roll/stellar" : "" ) : ""}
           className="custom-button text-white rounded-lg button"
         >
           <span>Roletar pokemon</span>
